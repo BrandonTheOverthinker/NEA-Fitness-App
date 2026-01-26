87fd50e066e29570debee22279ca8e03e6a1240a
brandonmattey <brandonmattey@A240570>
2026-01-26 14:05:32 +0000

    Add project files.

---
+++ NEA Fitness App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NEA_Fitness_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NEA Fitness App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NEA_Fitness_App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
