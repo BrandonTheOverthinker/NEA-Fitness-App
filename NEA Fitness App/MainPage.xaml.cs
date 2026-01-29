c9212749fcd6fc7bc1df343d819d5c51544a8cda
BRANDON MATTEY <BRANDONMATTEY@exe-coll.ac.uk>
2026-01-29 12:51:29 +0000

    Set up home page layout

--- NEA Fitness App/MainPage.xaml.cs
+++ NEA Fitness App/MainPage.xaml.cs
@@ -8,15 +8,6 @@
         {
             InitializeComponent();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            CounterBtn.Text = (count == 1) ? $"Clicked {count} time" : $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
     }
 
 }
