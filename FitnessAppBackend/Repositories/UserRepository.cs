7b4e62ec582de0f0c8892886e8858168f8eb3355
BrandonTheOverthinker <brandonmattey767@gmail.com>
2026-02-07 23:16:08 +0000

    Create user repository for accessing database with low coupling in the entire program

---
+++ FitnessAppBackend/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using FitnessAppBackend.Data;
+using FitnessAppBackend.Interfaces;
+using FitnessAppBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessAppBackend.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+        public UserRepository(AppDbContext context) => _context = context;
+
+        public async Task<User?> GetUserByUsernameAsync(string username) =>
+            await _context.Users.FirstOrDefaultAsync(u => u.UserName == username);
+
+        public async Task CreateUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> UserExistsAsync(string username) =>
+            await _context.Users.AnyAsync(u => u.UserName == username);
+
+        public async Task<int> GetUserCountAsync()
+        {
+            return await _context.Users.CountAsync();
+        }
+    }
+}
