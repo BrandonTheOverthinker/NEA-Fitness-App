d7beb49f32ef52e0edd6c36bbac4eb1ccc53fb84
BrandonTheOverthinker <brandonmattey767@gmail.com>
2026-02-07 23:14:46 +0000

    Create interface for user repository

---
+++ FitnessAppBackend/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using FitnessAppBackend.Models;
+
+namespace FitnessAppBackend.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetUserByUsernameAsync(string username);
+        Task CreateUserAsync(User user);
+        Task<bool> UserExistsAsync(string username);
+        Task<int> GetUserCountAsync();
+    }
+}
