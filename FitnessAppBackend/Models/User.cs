e901fa0f1e639cebb22af4fab76b49013aa5cb0b
BRANDON MATTEY <BRANDONMATTEY@exe-coll.ac.uk>
2026-02-10 12:52:33 +0000

    Create all remaining classes for database tables

--- FitnessAppBackend/Models/User.cs
+++ FitnessAppBackend/Models/User.cs
@@ -12,4 +12,64 @@ namespace FitnessAppBackend.Models
         [Required, MaxLength(255)]
         public string PasswordHash { get; set; } = string.Empty;
     }
+
+    public class Exercise
+    {
+
+    }
+    public class UserExercise
+    {
+
+    }
+    public class Workout
+    {
+
+    }
+    public class Set
+    {
+
+    }
+    public class ExerciseLog
+    {
+
+    }
+    public class UserPersonalRecord
+    {
+
+    }
+
+    public class Food
+    {
+
+    }
+    public class FoodLog
+    {
+
+    }
+    public class Macronutrients
+    {
+
+    }
+
+    public class UserGoal
+    {
+
+    }
+    public class ExerciseGoal
+    {
+
+    }
+    public class WeightGoal
+    {
+
+    }
+    public class NutritionGoal
+    {
+
+    }
+
+    public class XPLevel
+    {
+
+    }
 }
