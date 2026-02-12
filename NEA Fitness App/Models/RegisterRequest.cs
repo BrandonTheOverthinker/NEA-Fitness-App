3c6f115dfb5c8aad61e54eb23d973bd7fa5ab594
BrandonTheOverthinker <brandonmattey767@gmail.com>
2026-02-12 01:11:56 +0000

    add remaining User Properties to RegisterRequest

--- NEA Fitness App/Models/RegisterRequest.cs
+++ NEA Fitness App/Models/RegisterRequest.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NEAFitnessApp.Models
+﻿namespace NEAFitnessApp.Models
 {
-    public class RegisterRequest
+    public record RegisterRequest
     {
         public int UserID { get; set; } 
         public string UserName { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
-        
+        public DateOnly UserDOB { get; set; }
+        public decimal BodyWeight { get; set; }
+        public decimal Height { get; set; }
     }
 }
+// completed
