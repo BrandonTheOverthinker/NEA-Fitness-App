c17c60fa89d6c89052ff57e39f1421de8fd1eaaa
BRANDON MATTEY <BRANDONMATTEY@exe-coll.ac.uk>
2026-02-12 14:58:07 +0000

    trying to get registration page to work

--- NEA Fitness App/RegistrationPage.xaml.cs
+++ NEA Fitness App/RegistrationPage.xaml.cs
@@ -1,18 +1,28 @@
-namespace NEAFitnessApp;
+using System.Net.Http.Json;
+using NEAFitnessApp.Models;
 
-public partial class RegistrationPage : ContentPage
+namespace NEAFitnessApp
 {
-	public RegistrationPage()
+	public partial class RegistrationPage : ContentPage
 	{
-		InitializeComponent();
-	}
-	private void OnMetricsChanged(object sender, EventArgs e)
-	{
-        // >0 check for weight and height
+		public RegistrationPage() => InitializeComponent();
+
+		private void OnMetricsChanged(object sender, TextChangedEventArgs e)
+		{
+			// >0 check for weight and height
 			// BMI Calculation
-				// Result Output and Store Value
-    }
-    // OnFinalizeRegistrationClicked will send the registration data to the backend, and if successful, will navigate to the Home Page (AppShell)
-		// RegisterRequest code
+			// Result Output and Store Value
+			if (decimal.TryParse(WeightEntry.Text, out decimal weight) && decimal.TryParse(HeightEntry.Text, out decimal height) && height > 0)
+			{
+
+			}
+		}
+
+		private void OnFinaliseRegistrationClicked(object sender, EventArgs e)
+		{
+			// OnFinaliseRegistrationClicked will send the registration data to the backend, and if successful, will navigate to the Home Page (AppShell)
+			// RegisterRequest code
 			// validate response, display alert and navigate to loginscreen or main page
+		}
+	}
 }
