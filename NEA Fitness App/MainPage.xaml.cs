296fc11bbdc30d8c81536fda7426556aae8e09cd
BrandonTheOverthinker <brandonmattey767@gmail.com>
2026-02-15 20:35:17 +0000

    Updated MainPage to display maintenance calories

--- NEA Fitness App/MainPage.xaml.cs
+++ NEA Fitness App/MainPage.xaml.cs
@@ -2,11 +2,35 @@
 {
     public partial class MainPage : ContentPage
     {
-
         public MainPage()
         {
             InitializeComponent();
         }
-    }
 
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            string storedGoal = Preferences.Default.Get("LocalUserMaintenanceGoal", "Not Set");
+
+            if (storedGoal != "Not Set" && decimal.TryParse(storedGoal, out decimal goalValue))
+            {
+                MaintenanceCalorieLabel.Text = goalValue.ToString("F0");
+
+                decimal current = 0m;
+                CurrentCaloriesLabel.Text = current.ToString("F0");
+
+                if (goalValue > 0)
+                {
+                    CalorieProgressBar.Progress = (double)(current / goalValue);
+                }
+            }
+            else
+            {
+                MaintenanceCalorieLabel.Text = "Not Set";
+            }
+
+            XpDisplay.Text = "0";
+        }
+    }
 }
