94a7b812b66d6a6e71c10404d6870943abcca02b
BrandonTheOverthinker <brandonmattey767@gmail.com>
2026-02-23 18:28:19 +0000

    added frontend fooditem request logic

---
+++ NEA Fitness App/Models/FoodItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEAFitnessApp.Models
+{
+    public class FoodItem
+    {
+        public int FoodItemId { get; set; }
+        public string FoodName { get; set; } = string.Empty;
+        public string? Barcode { get; set; }
+
+        public int Calories { get; set; }
+        public decimal Protein { get; set; }
+        public decimal Fat { get; set; }
+        public decimal SaturatedFat { get; set; }
+        public decimal Carbohydrates { get; set; }
+        public decimal Sugar { get; set; }
+        public decimal Fibre { get; set; }
+
+        public int? CreatedByUserID { get; set; }
+    }
+}
