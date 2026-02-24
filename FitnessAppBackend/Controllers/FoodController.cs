c79cbd4302e608bc3bbb62333da7171710f5fcaf
BRANDON MATTEY <BRANDONMATTEY@exe-coll.ac.uk>
2026-02-24 13:04:23 +0000

    Updated food controller syntax

--- FitnessAppBackend/Controllers/FoodController.cs
+++ FitnessAppBackend/Controllers/FoodController.cs
@@ -8,30 +8,29 @@ namespace FitnessAppBackend.Controllers
     [Route("api/[controller]")]
     public class FoodController : ControllerBase
     {
-        private readonly IFoodRepository _foodRepo;
-        public FoodController(IFoodRepository foodRepo) { _foodRepo = foodRepo; }
+        private readonly IFoodRepository foodRepo;
+        public FoodController(IFoodRepository foodRepo) => this.foodRepo = foodRepo;
 
         [HttpGet("all")]
-        public async Task<IActionResult> GetAllFoods() => Ok(await _foodRepo.GetAllFoodsAsync());
+        public async Task<IActionResult> GetAllFoods() => Ok(await foodRepo.GetAllFoodsAsync());
 
         [HttpPost("create")]
         public async Task<IActionResult> CreateFood([FromBody] FoodItem newFood)
         {
-            var created = await _foodRepo.AddFoodAsync(newFood);
+            var created = await foodRepo.AddFoodAsync(newFood);
             return Ok(created);
         }
 
         [HttpGet("logs/{userId}/{date}")]
         public async Task<IActionResult> GetDailyLogs(int userId, DateTime date)
         {
-            var logs = await _foodRepo.GetLogsByDateAsync(userId, date);
+            var logs = await foodRepo.GetLogsByDateAsync(userId, date);
             return Ok(logs);
         }
         [HttpGet("weekly/{userId}/{startDate}")]
         public async Task<IActionResult> GetWeeklyLogs(int userId, DateTime startDate)
         {
-            // This calls your repository method we fixed earlier
-            var logs = await _foodRepo.GetWeeklyLogsAsync(userId, startDate);
+            var logs = await foodRepo.GetWeeklyLogsAsync(userId, startDate);
             return Ok(logs);
         }
     }
