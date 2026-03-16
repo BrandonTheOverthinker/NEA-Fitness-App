0606e1a314a8be3df570141b7c1506869edbf8b1
BrandonTheOverthinker <brandonmattey767@gmail.com>
2026-03-16 22:32:49 +0000

    registered routes for pages that are navigated to out of app shell

--- NEA Fitness App/AppShell.xaml.cs
+++ NEA Fitness App/AppShell.xaml.cs
@@ -5,6 +5,11 @@
         public AppShell()
         {
             InitializeComponent();
+
+            // Register routes for pages that are navigated to programmatically but are NOT flyout items (i.e. pushed onto the stack mid-session):
+            Routing.RegisterRoute(nameof(CreateExercisePage), typeof(CreateExercisePage));
+            Routing.RegisterRoute(nameof(ActiveWorkoutPage), typeof(ActiveWorkoutPage));
+            Routing.RegisterRoute(nameof(ExerciseHistoryPage), typeof(ExerciseHistoryPage));
         }
     }
 }
